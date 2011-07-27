83955106a6187b3d6402381209c5a81470a3b68b
Emma Sax <emma.sax@gmail.com>
2011-07-27 23:12:34 +0100

    putting them into a rough circle

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ $(function() {
 	var info = $('.info');
 	var everyone = $('li[data-refs]');
 
+	$('img').after('<img class="mask" src="images/mask.png" width="70" height="70" />');
+
+
 	everyone.mouseenter(function(){
 		var $this = $(this);
 		info.find('ul, h3').remove();
 		$('.highlighted').removeClass('highlighted');
+		$('.red').remove();
 		$this.addClass('highlighted');
+
+		$this.append('<img class="mask red" src="images/highlight.png" width="70" height="70" />');
 		
 		$this.find('h3').clone().appendTo(info);
 		$this.find('ul').clone().appendTo(info);
@@ -15,7 +21,6 @@ $(function() {
 		var temp = $this.attr('data-refs');
 		var web = temp.split(' ');
 
-		// console.log(everyone);
 
 		for(var i=0; i < web.length; i++) {
 			// console.log('items relationships: ' + web[i]);
